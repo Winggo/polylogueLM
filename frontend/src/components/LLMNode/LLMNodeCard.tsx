ba424420c841bd7f19b4adc6a2c7cc4bfcfdda2d
Winggo Tse <winggo.tse@gmail.com>
2026-01-11 20:44:50 -0800

    simplify node empty state

--- frontend/src/components/LLMNode/LLMNodeCard.tsx
+++ frontend/src/components/LLMNode/LLMNodeCard.tsx
@@ -1,17 +1,13 @@
 export default function LLMNodeCard() {
     return (
         <div className="w-full h-full flex flex-col items-center justify-around llm-node-card">
-            <div className="cursor-text select-text text-center mt-[0px]">
-                <span>Press ⇥ to use suggested prompt</span>
-                <br />
-                <span>Click on → icon to start an alternate dialogue</span>
-                <br />
+            <div className="cursor-text select-text text-center mb-[50px]">
+                <span><i>Visualize and explore branching LLM conversations</i></span>
+                <br /><br />
                 <span>Click on ⋮⋮⋮ and drag to move node</span>
                 <br />
-                <span>Press ⌫ to delete selected node</span>
-            </div>
-            
-            <div className="cursor-text select-text mb-[50px]">
+                <span><i>Backspace</i> to delete selected node</span>
+                <br /><br /><br /><br /><br /><br /><br /><br />
                 <span>You can...</span>
                 <br />
                 <span>- create a packing list 🏕️</span>
